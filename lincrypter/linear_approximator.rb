8be4657b21e9294c5429c7f75a965be1b962155d
Keith Gould <keithmgould@gmail.com>
2013-05-24 19:29:49 -0400

    linear approximator summing correctly.

--- lincrypter/linear_approximator.rb
+++ lincrypter/linear_approximator.rb
@@ -1,18 +1,36 @@
 class LinearApproximator
   def self.determine_best
-    #compute all sbox subs first
-    sbox_results = []
-    16.times.each do |input|
-      sbox_results <<  Sbox.sub()
-    end
-    #then run through each pair of maskings
-    16.times.each do |input|
-      16.times.each do |output|
-        input_mask = input.to_s(2).split("").map(&:to_i).inject(0) {|sum,x| sum += x}
-        output_mask = output.to_s(2).split("").map(&:to_i).inject(0) {|sum,x| sum += x}
-        puts "#{input_mask} :: #{output_mask}"
+    matches = {}
+    16.times.each do |input| # 0 - 15
+      output = SBOXFORWARD[input]
+      16.times.each do |input_mask|
+        input_xor_mask_sum = xor_mask_sum(input_mask, input)
+        16.times.each do |output_mask|
+          output_xor_mask_sum = xor_mask_sum(output_mask, output)
+          if input_xor_mask_sum == output_xor_mask_sum
+            if matches[[input_mask, output_mask]]
+              matches[[input_mask, output_mask]] += 1
+            else
+              matches[[input_mask, output_mask]] = 1
+            end
+          end
+        end
       end
     end
+    matches.each_pair do |masks, sum|
+      puts "i: #{masks[0]}, o: #{masks[1]}, sum: #{sum}"
+    end
+  end
 
+  private
+
+  def self.xor_mask_sum(mask, subject)
+    mask_array = mask.to_s(2).split("").map(&:to_i)
+    subject_array = subject.to_s(2).split("").map(&:to_i)
+    total = 0
+    4.times do |i|
+      total = total ^ subject[i] if mask[i] == 1
+    end
+    total
   end
 end
